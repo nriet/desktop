b66d21aa5c376f14b1f348ba84e6bb753ad9e782
nriet <itzyx@vip.qq.com>
2023-05-24 23:38:38 +0800

    dataTransfer

--- src/ubuntu/install/dataTransfer/install_dataTransfer.sh
+++ src/ubuntu/install/dataTransfer/install_dataTransfer.sh
@@ -2,8 +2,9 @@
 
 cd /tmp
 wget -q -O dataTransfer.tar.gz "http://bt.nriet.xyz/down/PkG2YIRGBC3o.gz"
+mkdir /opt/dataTransfer
 tar -xzf dataTransfer.tar.gz -C /opt/dataTransfer
-
+rm -rf /tmp/*
 #ECLIPSE_ICON="/opt/dataTransfer/plugins/$(ls /opt/dataTransfer/plugins/ | grep -m 1 org.dataTransfer.platform_)/dataTransfer.png"
 #sed -i "s#dataTransfer.png#${ECLIPSE_ICON}#" $INST_SCRIPTS/dataTransfer/dataTransfer.desktop
 cp $INST_SCRIPTS/dataTransfer/dataTransfer.desktop $HOME/Desktop/
