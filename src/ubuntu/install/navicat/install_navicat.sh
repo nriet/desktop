961a37d45622f93cf4a49f1e49530bd172933752
nriet <itzyx@vip.qq.com>
2023-06-20 17:48:14 +0800

    navicat

--- src/ubuntu/install/navicat/install_navicat.sh
+++ src/ubuntu/install/navicat/install_navicat.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -ex
-cp navicat.desktop /usr/share/applications/navicat.desktop
+
+echo "Navicat installation"
+
+cp /dockerstartup/install/navicat/navicat.desktop /usr/share/applications/navicat.desktop
 mkdir /opt/Navicat15
-cp navicat.png /opt/Navicat15/navicat.png
+cp /dockerstartup/install/navicat/navicat.png /opt/Navicat15/navicat.png
 wget -q http://bt.nriet.xyz/down/yhtrxHxQj7AK.AppImage -O /opt/Navicat15/navicat15-premium-cs-pathed.AppImage
 chmod -R +x /opt/Navicat15/navicat15-premium-cs-pathed.AppImage
 #断网后填入生成的注册码，超时后选择手动激活，输入请求码，两次回车生成激活码，填入后即可激活成功。
