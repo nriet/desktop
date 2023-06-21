2dd27dc89e24bb644aefc677ecadaa86fec34198
nriet <itzyx@vip.qq.com>
2023-06-21 13:19:02 +0800

    eclipse tomcat

--- src/ubuntu/install/eclipse/install_eclipse.sh
+++ src/ubuntu/install/eclipse/install_eclipse.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ECLIPSE_VER_DATE="2023-06"
+TOMCAT_VER="8.5.90"
 
 cd /tmp
 wget -q -O eclipse.tar.gz "https://mirrors.xmission.com/eclipse/technology/epp/downloads/release/${ECLIPSE_VER_DATE}/R/eclipse-jee-${ECLIPSE_VER_DATE}-R-linux-gtk-$(arch).tar.gz"
@@ -10,3 +11,10 @@ ECLIPSE_ICON="/opt/eclipse/plugins/$(ls /opt/eclipse/plugins/ | grep -m 1 org.ec
 sed -i "s#eclipse128.png#${ECLIPSE_ICON}#" $INST_SCRIPTS/eclipse/eclipse.desktop
 cp $INST_SCRIPTS/eclipse/eclipse.desktop $HOME/Desktop/
 cp $INST_SCRIPTS/eclipse/eclipse.desktop /usr/share/applications/
+
+rm -rf /tmp/eclipse.tar.gz
+
+wget -q -O tomcat.tar.gz "https://dlcdn.apache.org/tomcat/tomcat-8/v${TOMCAT_VER}/bin/apache-tomcat-${TOMCAT_VER}.tar.gz"
+tar -xzf tomcat.tar.gz -C /opt
+
+rm -rf /tmp/tomcat.tar.gz
