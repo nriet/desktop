0bf0cabb8ad5aac8beefbc031b64cb07872f4ec3
nriet <itzyx@vip.qq.com>
2023-06-21 11:08:29 +0800

    jee

--- src/ubuntu/install/eclipse/install_eclipse.sh
+++ src/ubuntu/install/eclipse/install_eclipse.sh
@@ -3,7 +3,7 @@
 ECLIPSE_VER_DATE="2023-06"
 
 cd /tmp
-wget -q -O eclipse.tar.gz "https://mirrors.xmission.com/eclipse/technology/epp/downloads/release/${ECLIPSE_VER_DATE}/R/eclipse-java-${ECLIPSE_VER_DATE}-R-linux-gtk-$(arch).tar.gz"
+wget -q -O eclipse.tar.gz "https://mirrors.xmission.com/eclipse/technology/epp/downloads/release/${ECLIPSE_VER_DATE}/R/eclipse-jee-${ECLIPSE_VER_DATE}-R-linux-gtk-$(arch).tar.gz"
 tar -xzf eclipse.tar.gz -C /opt
 
 ECLIPSE_ICON="/opt/eclipse/plugins/$(ls /opt/eclipse/plugins/ | grep -m 1 org.eclipse.platform_)/eclipse128.png"
