ec3e1f741d191e4c3377241ddfed7a881776573a
axiu <22583574@qq.com>
2023-12-07 14:03:22 +0800

    Update install_dataTransfer.sh

--- src/ubuntu/install/dataTransfer/install_dataTransfer.sh
+++ src/ubuntu/install/dataTransfer/install_dataTransfer.sh
@@ -4,6 +4,7 @@ cd /tmp
 wget -q -O dataTransfer.tar.gz "http://bt.nriet.xyz/down/HeEuh4gm4oWD.gz?fname=/DataTransfer_7.9.2_centos7_binary.tar.gz&down=true"
 mkdir /opt/dataTransfer
 tar -xzf dataTransfer.tar.gz -C /opt/dataTransfer
+chmod +X /opt/dataTransfer/bin/protect.sh
 rm -rf /tmp/*
 #ECLIPSE_ICON="/opt/dataTransfer/plugins/$(ls /opt/dataTransfer/plugins/ | grep -m 1 org.dataTransfer.platform_)/dataTransfer.png"
 #sed -i "s#dataTransfer.png#${ECLIPSE_ICON}#" $INST_SCRIPTS/dataTransfer/dataTransfer.desktop
